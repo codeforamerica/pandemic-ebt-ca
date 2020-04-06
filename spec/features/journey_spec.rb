46b155ce061d2e1f2cd99a77fb6b57a83847ac3e
Ben Calegari <bcalegari@codeforamerica.org>
2020-04-06 15:06:56 -0700

    Copy changes and style adjustments to home and how page
    [#172110721]
    [#172110788]
    Signed-off-by: Luigi Ray-Montanez <luigi@codeforamerica.org>

--- spec/features/journey_spec.rb
+++ spec/features/journey_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Journey", type: :feature do
     it "should run the full journey" do
       visit "/"
       expect(page).to have_text "Get money to buy healthy food while schools are closed."
-      click_on "Apply now"
+      click_on "Sign up now"
       expect(page).to have_text "Here's how it works:"
       click_on "Continue"
     end
