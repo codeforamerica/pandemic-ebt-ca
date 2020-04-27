2fb04498e91be707ab4aad3ea880aadb2b3490c3
Ben Calegari <bcalegari@codeforamerica.org>
2020-04-27 16:55:21 -0700

    Implement GA on prod only
    [#172110973]

--- app/assets/javascripts/googleanalytics.js
+++
@@ -1,6 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-20825280-44');
