6933307ae195cce4efe46b87385359c11dc63703
Ben Calegari <bcalegari@codeforamerica.org>
2020-04-27 16:02:21 -0700

    Add GA
    [#172110973]

---
+++ app/assets/javascripts/googleanalytics.js
@@ -0,0 +1,6 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-20825280-44');
