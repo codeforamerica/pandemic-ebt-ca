c0298d0cf637c532ac8a68929e148d4cd81a5380
Matthew Schultz <abstractapproach@gmail.com>
2020-04-30 10:14:09 -0500

    turn on caching in test so that tests will use it

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,21 +23,13 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Enable/disable caching. By default caching is disabled.
-  # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
-    config.action_controller.enable_fragment_cache_logging = true
-
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
-    }
-  else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
-  end
+  # Turn ON caching, since there are several tests that actually test caching:
+  config.action_controller.perform_caching = true
+  config.action_controller.enable_fragment_cache_logging = true
+  config.cache_store = :memory_store
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, max-age=#{2.days.to_i}"
+  }
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
