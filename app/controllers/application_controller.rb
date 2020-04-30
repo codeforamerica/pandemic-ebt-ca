98f1f14959904a268a91988de8d4523e49febd69
Luigi Ray-Montanez <luigi@codeforamerica.org>
2020-04-30 18:31:14 -0400

    Implement Spanish and Chinese translations for /info
    [#172390176]
    
    Co-authored-by: Alex Gonzalez <agonzalez@codeforamerica.org>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    if I18n.locale != :en
+      { locale: I18n.locale }
+    else
+      {}
+    end
+  end
 end
---
+++ spec/features/info_page_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Info page', type: :feature do
+  it 'switches language to Spanish' do
+    visit '/info'
+    click_on 'Español'
+    expect(page).to have_text 'Información sobre P-EBT'
+  end
+end
