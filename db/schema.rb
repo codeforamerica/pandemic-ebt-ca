45954445ab7c2306363c97398a4227de9f1988bb
Matthew Schultz <abstractapproach@gmail.com>
2020-05-01 17:09:17 -0500

    Add selection for public/private school
    [#172422168]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_163007) do
+ActiveRecord::Schema.define(version: 2020_05_01_220239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_04_24_163007) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "suid"
+    t.integer "school_type", limit: 2
     t.index ["household_id"], name: "index_children_on_household_id"
   end
 
--- spec/forms/children_form_spec.rb
+++ spec/forms/children_form_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe ChildrenForm do
   before do
     @household = Household.create(is_eligible: :yes)
+    @valid_form = described_class.new(@household, { first_name: 'Jane', last_name: 'Smith', dob_month: '12', dob_day: '12', dob_year: '2010', school_type: 'public_school' })
   end
 
   after do
@@ -11,8 +12,7 @@ describe ChildrenForm do
 
   describe '#save' do
     it 'saves one child to the household' do
-      form = described_class.new(@household, { first_name: 'Jane', last_name: 'Smith',
-                                               dob_month: '01', dob_day: '09', dob_year: '2010' })
+      form = @valid_form.dup
       form.valid?
       expect { form.save }.not_to change(Household, :count)
       expect { form.save }.to change { @household.children.count }.by(1)
@@ -21,32 +21,58 @@ describe ChildrenForm do
       @household.children.first
 
       expect(@household.children.first.first_name).to eq('Jane')
-      expect(@household.children.first.dob).to eq(Date.parse('01/09/2010'))
+      expect(@household.children.first.dob).to eq(Date.parse('12/12/2010'))
       expect(@household.children.first.suid).to be_present
     end
   end
 
   describe '#presence_of_dob_fields' do
-    it 'is invalid if any dob field is present' do
-      form = described_class.new(@household, { first_name: 'Jane', last_name: 'Smith', dob_month: '', dob_day: '09', dob_year: '2010' })
+    it 'is invalid if any dob field is not present' do
+      form = @valid_form.dup
+      form.dob_day = ''
       expect(form).not_to be_valid
 
-      form = described_class.new(@household, { first_name: 'Jane', last_name: 'Smith', dob_month: '12', dob_day: '', dob_year: '2010' })
+      form = @valid_form.dup
+      form.dob_month = ''
       expect(form).not_to be_valid
 
-      form = described_class.new(@household, { first_name: 'Jane', last_name: 'Smith', dob_month: '12', dob_day: '12', dob_year: '' })
+      form = @valid_form.dup
+      form.dob_year = ''
       expect(form).not_to be_valid
     end
 
     it 'is valid if all dob fields are present' do
-      form = described_class.new(@household, { first_name: 'Jane', last_name: 'Smith', dob_month: '12', dob_day: '12', dob_year: '2010' })
+      form = @valid_form.dup
       expect(form).to be_valid
     end
 
-    it 'onlies show one dob error if any of the fields are blank' do
-      form = described_class.new(@household, { first_name: 'Jane', last_name: 'Smith', dob_month: '', dob_day: '12', dob_year: '2020' })
+    it 'only shows one dob error if any of the fields are blank' do
+      form = @valid_form.dup
+      form.dob_day = ''
       form.valid?
       expect(form.errors.count).to eq(1)
+    end
+  end
+
+  describe '#presence_of_school_type_field' do
+    it 'is invalid if school type is not present' do
+      form = @valid_form.dup
+      form.school_type = ''
+      expect(form).not_to be_valid
+    end
+
+    it 'is invalid if school type is not one of the accepted types' do
+      form = @valid_form.dup
+      form.school_type = 'obviously_invalid_response'
+      expect(form).not_to be_valid
+    end
+
+    it 'has a relevant error message' do
+      form = @valid_form.dup
+      form.school_type = ''
+      form.valid?
+      expect(form.errors.count).to eq(1)
+      expect(form.errors.first[1]).to eq('Please select which type of school they attend.')
     end
   end
 end
