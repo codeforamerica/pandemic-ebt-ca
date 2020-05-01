813c73b45e0006676a5e8547b3e89ccb1d52657d
Matthew Schultz <abstractapproach@gmail.com>
2020-05-01 14:21:36 -0500

    Woot I got it to work and stuff!
    [#172593056]

--- app/forms/form_navigation.rb
+++ app/forms/form_navigation.rb
@@ -6,6 +6,7 @@ class FormNavigation
     ReceivedCardController,
     AlreadyReceivedController,
     ChildrenController,
+    JustSoYouKnowController,
     ParentController,
     ResidentialAddressController,
     MailingAddressController,
