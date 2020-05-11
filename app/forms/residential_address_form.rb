1e38d96f69c551e3fcd6a100ba44493c24e326f4
Matthew Schultz <abstractapproach@gmail.com>
2020-05-11 10:05:14 -0500

    Set language in initial form
    [#172740288]

--- app/forms/residential_address_form.rb
+++ app/forms/residential_address_form.rb
@@ -1,10 +1,11 @@
 class ResidentialAddressForm < Form
-  set_attributes_for :household, :residential_street, :residential_street_2, :residential_city, :residential_zip_code, :has_mailing_address
+  set_attributes_for :household, :residential_street, :residential_street_2, :residential_city, :residential_zip_code, :has_mailing_address, :language
   validates_presence_of :residential_street, message: 'Please fill in the street address.'
   validates :residential_street_2, length: { maximum: 128, too_long: 'Please enter a shorter unit or apartment.' }
   validates_presence_of :residential_city, message: 'Please fill in the city.'
   validates_presence_of :residential_zip_code, message: 'Please fill in the ZIP code.'
   validates_presence_of :has_mailing_address, message: 'Please choose where to send mail.'
+  validates :language, inclusion: { in: I18n.available_locales.map(&:to_s) }
 
   def save
     household.update(attributes_for(:household))
---
+++ config/initializers/locale.rb
@@ -0,0 +1,3 @@
+# Configure I18n
+I18n.available_locales = [:en, :es, :zh]
+I18n.default_locale = :en
