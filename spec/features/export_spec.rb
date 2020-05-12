4d07344ade37296280a8a3faf50efb13dc4fec77
Matthew Schultz <abstractapproach@gmail.com>
2020-05-12 18:25:11 -0500

    Save email/phone number in CSV (#17)
    [#172336953](https://www.pivotaltracker.com/story/show/172336953)

--- spec/features/export_spec.rb
+++ spec/features/export_spec.rb
@@ -5,8 +5,14 @@ Rails.application.load_tasks
 HEADERS = %w[suid household_id student_first_name student_last_name student_dob student_school_type parent_signature residential_street residential_street_2 residential_city residential_state residential_zip_code mailing_street mailing_street_2 mailing_city mailing_state mailing_zip_code phone_number email_address language submitted_at application_experience confirmation_code].freeze
 
 RSpec.describe 'Exporting Children as CSV', type: :feature do
+  def row_for_child(csv_data, child)
+    csv_data.find { |r| r['suid'] == child.suid }
+  end
+
   before(:all) do
     @output_file_name = Rails.root.join('tmp', 'all.csv')
+    @child_with_email = create(:child, household_id: create(:household, :with_email).id)
+    @child_with_phone_number = create(:child, household_id: create(:household, :with_phone_number).id)
     create_list(:child, 20)
     File.delete(@output_file_name) if File.exist?(@output_file_name)
 
@@ -16,6 +22,12 @@ RSpec.describe 'Exporting Children as CSV', type: :feature do
     $stdout = @captured_stdout
     Rake::Task['export:csv:all'].invoke
     $stdout = @original_stdout
+  end
+
+  after(:all) do
+    # prevent data leakage:
+    Child.destroy_all
+    Household.destroy_all
   end
 
   it 'Shows a confirmation message on the console' do
@@ -39,5 +51,19 @@ RSpec.describe 'Exporting Children as CSV', type: :feature do
   it 'Fills all mailing addresses, duplicating residential data where required' do
     csv_data = CSV.read(@output_file_name, headers: true)
     expect(csv_data.map { |r| r['mailing_street'] }).to all(be_present)
+  end
+
+  it 'Exports email address if present' do
+    csv_data = CSV.read(@output_file_name, headers: true)
+    email_row = row_for_child(csv_data, @child_with_email)
+    expect(@child_with_email.household.email_address).to be_present
+    expect(email_row['email_address']).to eq(@child_with_email.household.email_address)
+  end
+
+  it 'Exports phone number if present' do
+    csv_data = CSV.read(@output_file_name, headers: true)
+    phone_row = row_for_child(csv_data, @child_with_phone_number)
+    expect(@child_with_phone_number.household.phone_number).to be_present
+    expect(phone_row['phone_number']).to eq(@child_with_phone_number.household.phone_number)
   end
 end
