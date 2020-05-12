23abdf720a7ac596c5eb63f1e27d7634a3567aaa
Ben Calegari <bcalegari@codeforamerica.org>
2020-05-12 16:39:13 -0700

    Add homelessness question to registered address form
    - If selected, this will clear out any existing answers for the rest of
      the form and not require them
    
    [#172730309]

--- app/forms/residential_address_form.rb
+++ app/forms/residential_address_form.rb
@@ -1,12 +1,16 @@
 class ResidentialAddressForm < Form
-  set_attributes_for :household, :residential_street, :residential_street_2, :residential_city, :residential_zip_code, :has_mailing_address
-  validates_presence_of :residential_street, message: 'Please fill in the street address.'
-  validates :residential_street_2, length: { maximum: 128, too_long: 'Please enter a shorter unit or apartment.' }
-  validates_presence_of :residential_city, message: 'Please fill in the city.'
-  validates_presence_of :residential_zip_code, message: 'Please fill in the ZIP code.'
-  validates_presence_of :has_mailing_address, message: 'Please choose where to send mail.'
+  set_attributes_for :household, :residential_street, :residential_street_2, :residential_city, :residential_zip_code, :has_mailing_address, :registered_homeless
+  validates_presence_of :residential_street, message: 'Please fill in the street address.', if: :registered_homeless_unfilled?
+  validates :residential_street_2, length: { maximum: 128, too_long: 'Please enter a shorter unit or apartment.' }, if: :registered_homeless_unfilled?
+  validates_presence_of :residential_city, message: 'Please fill in the city.', if: :registered_homeless_unfilled?
+  validates_presence_of :residential_zip_code, message: 'Please fill in the ZIP code.', if: :registered_homeless_unfilled?
+  validates_presence_of :has_mailing_address, message: 'Please choose where to send mail.', if: :registered_homeless_unfilled?
 
   def save
     household.update(attributes_for(:household))
+  end
+
+  def registered_homeless_unfilled?
+    registered_homeless == 'unfilled'
   end
 end
---
+++ spec/features/residential_address_page_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Residential address page', type: :feature do
+  describe 'deselecting homeless checkbox', :js do
+    it 'allows you to continue without selecting' do
+      visit '/en/steps/residential-address'
+      expect(page).to have_text 'What address are you registered at the school with?'
+      check 'Student was homeless at time of registration'
+      expect(page).not_to have_text('What is the street address?')
+      expect(page).not_to have_text('What is the unit or apartment (optional)?')
+      expect(page).not_to have_text('What is the city?')
+      expect(page).not_to have_text('What is the ZIP code?')
+      expect(page).not_to have_text('Is this the best place for you to get mail for the next two weeks?')
+      click_on 'Continue'
+      expect(page).to have_text('If there is a problem with your application, how would you like to be contacted?')
+    end
+  end
+end
