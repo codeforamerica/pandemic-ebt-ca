d48318249eeb3540e0e1881766a215e3d9282aea
Luigi Ray-Montanez <luigi@codeforamerica.org>
2020-05-13 16:25:51 -0400

    Implement closing the early experiment

---
+++ spec/features/experiment_over_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Early Screen', type: :feature do
+  before do
+    stub_const 'ENV', ENV.to_h.merge('COMING_SOON' => '1')
+    stub_const 'ENV', ENV.to_h.merge('EXPERIMENT_OVER' => '1')
+  end
+
+  describe 'Early page when experiment is over' do
+    it 'does show apply now when the environment variable is set' do
+      visit '/early'
+      expect(page).to have_text 'This test link is now closed'
+    end
+  end
+end
