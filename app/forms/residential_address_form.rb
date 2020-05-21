d9a93d3ffd7f8d526fa63ece313b3c8757a74164
Matthew Schultz <abstractapproach@gmail.com>
2020-05-21 11:14:23 -0500

    zip code validation

--- app/forms/residential_address_form.rb
+++ app/forms/residential_address_form.rb
@@ -3,8 +3,8 @@ class ResidentialAddressForm < Form
   validates_presence_of :residential_street, message: 'Please fill in the street address.', if: :registered_homeless_unfilled?
   validates :residential_street_2, length: { maximum: 128, too_long: 'Please enter a shorter unit or apartment.' }, if: :registered_homeless_unfilled?
   validates_presence_of :residential_city, message: 'Please fill in the city.', if: :registered_homeless_unfilled?
-  validates_presence_of :residential_zip_code, message: 'Please fill in the ZIP code.', if: :registered_homeless_unfilled?
   validates_presence_of :has_mailing_address, message: 'Please choose where to send mail.', if: :registered_homeless_unfilled?
+  validates :residential_zip_code, inclusion: { in: VALID_ZIP_CODES, message: 'Please fill in a valid CA ZIP code.' }, if: :registered_homeless_unfilled?
 
   def save
     household.update(attributes_for(:household))
