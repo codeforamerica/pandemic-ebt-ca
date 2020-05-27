d641d26db36e181eba2a21a65b3c7c65b85cc51b
Cory Foy <github@cornetdesign.com>
2020-05-27 13:49:58 -0400

    Adds redirect for success page if no children set up

--- app/controllers/residential_address_controller.rb
+++ app/controllers/residential_address_controller.rb
@@ -1,6 +1,7 @@
 class ResidentialAddressController < FormsController
   def edit
     if current_household.children.count.zero?
+      flash[:errors] = [I18n.t('validations.please_add_student')]
       redirect_to(children_steps_path)
       return
     end
