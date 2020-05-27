e5191f8408e572e217f5efdfd96c5611d4359a48
Cory Foy <github@cornetdesign.com>
2020-05-27 13:49:58 -0400

    Adds redirect for success page if no children set up

--- spec/controllers/success_controller_spec.rb
+++ spec/controllers/success_controller_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe SuccessController do
   it_behaves_like 'form controller always shows'
+
+  context 'when no children have been set up' do
+    before do
+      household = Household.create({ is_eligible: :yes })
+      session[:current_household_id] = household.id
+    end
+
+    describe '#edit' do
+      it 'redirects to the add children screen' do
+        get :edit, params: { locale: I18n.default_locale }
+
+        expect(response.code).to eq '302'
+      end
+    end
+  end
 end
