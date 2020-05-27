d211a424df62492dde7f2090ba82ee23d4b9a216
Matthew Schultz <abstractapproach@gmail.com>
2020-05-27 15:46:11 -0500

    Fix has_mailing_address getting erased bug

--- spec/forms/mailing_address_form_spec.rb
+++ spec/forms/mailing_address_form_spec.rb
@@ -18,24 +18,33 @@ describe MailingAddressForm do
       expect(household.mailing_zip_code).to eq('96021')
     end
 
+    it 'does not remove has_mailing_address' do
+      household = Household.create(is_eligible: :yes, has_mailing_address: :yes)
+      form = described_class.new(household, { mailing_street: '123 Elm Street', mailing_city: 'Oakland',
+                                              mailing_zip_code: '96021' })
+      form.save
+      household.reload
+      expect(household.has_mailing_address).to eq('yes')
+    end
+
     it 'is valid without address 2 line' do
       household = Household.create(is_eligible: :yes)
       form = described_class.new(household, { mailing_street: '123 Elm Street', mailing_city: 'Oakland',
-                                              mailing_zip_code: '96021', has_mailing_address: 'yes' })
+                                              mailing_zip_code: '96021' })
       expect(form).to be_valid
     end
 
     it 'is valid with address 2 line' do
       household = Household.create(is_eligible: :yes)
       form = described_class.new(household, { mailing_street: '123 Elm Street', mailing_street_2: 'Apt 2', mailing_city: 'Oakland',
-                                              mailing_zip_code: '96021', has_mailing_address: 'yes' })
+                                              mailing_zip_code: '96021' })
       expect(form).to be_valid
     end
 
     it 'is not valid if address 2 is too long' do
       household = Household.create(is_eligible: :yes)
       form = described_class.new(household, { mailing_street: '123 Elm Street', mailing_city: 'Oakland',
-                                              mailing_zip_code: '96021', has_mailing_address: 'yes', mailing_street_2: Faker::String.random(length: 129) })
+                                              mailing_zip_code: '96021', mailing_street_2: Faker::String.random(length: 129) })
       expect(form).not_to be_valid
       expect(form.errors.first[1]).to eq('Please enter a shorter unit or apartment.')
     end
@@ -43,7 +52,7 @@ describe MailingAddressForm do
     it 'validates zip codes' do
       household = Household.create(is_eligible: :yes)
       form = described_class.new(household, { mailing_street: '123 Elm Street', mailing_city: 'Oakland',
-                                              mailing_zip_code: 'not a zip code', has_mailing_address: 'yes' })
+                                              mailing_zip_code: 'not a zip code' })
 
       expect(form).not_to be_valid
     end
@@ -51,7 +60,7 @@ describe MailingAddressForm do
     it 'validates that zip codes are from CA' do
       household = Household.create(is_eligible: :yes)
       form = described_class.new(household, { mailing_street: '1600 Pennsylvania Avenue NW', mailing_city: 'Washington D.C.',
-                                              mailing_zip_code: '20500', has_mailing_address: 'yes' })
+                                              mailing_zip_code: '20500' })
 
       expect(form).not_to be_valid
     end
