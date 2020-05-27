3b6129e008e3477c7af867e5638597b3915b2e36
Matthew Schultz <abstractapproach@gmail.com>
2020-05-27 15:46:11 -0500

    Fix has_mailing_address getting erased bug

--- app/forms/mailing_address_form.rb
+++ app/forms/mailing_address_form.rb
@@ -1,5 +1,5 @@
 class MailingAddressForm < Form
-  set_attributes_for :household, :mailing_street, :mailing_street_2, :mailing_city, :mailing_zip_code, :has_mailing_address
+  set_attributes_for :household, :mailing_street, :mailing_street_2, :mailing_city, :mailing_zip_code
   validates_presence_of :mailing_street, message: proc { I18n.t('validations.address') }
   validates :mailing_street_2, length: { maximum: 128, too_long: proc { I18n.t('validations.address_2') } }
   validates_presence_of :mailing_city, message: proc { I18n.t('validations.city') }
