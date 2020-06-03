f5fd9840fbf6f5e18eeb27af6f83e68ed7e93588
Luigi Ray-Montanez <luigi@codeforamerica.org>
2020-06-03 17:51:48 -0400

    Escape double quotes when outputting CSV

--- app/helpers/csv_helper.rb
+++ app/helpers/csv_helper.rb
@@ -1,5 +1,6 @@
 module CsvHelper
   def csv_field(txt, options = { last: false })
+    txt = txt.to_s.gsub(/\"/, '""')
     "\"#{txt}\"#{options[:last] ? "\n" : ','}".html_safe
   end
 end
--- spec/features/export_spec.rb
+++ spec/features/export_spec.rb
@@ -21,6 +21,7 @@ RSpec.describe 'Exporting Children as CSV', type: :feature do
     @child_without_mailing_address = create(:child, household_id: create(:household, :without_mailing_address).id)
     @child_from_today = create(:child, household: create(:household, :submitted_today))
     @child_from_yesterday = create(:child, household: create(:household, :submitted_yesterday))
+    @child_with_double_quotes = create(:child, household: create(:household, residential_street_2: 'Apt "B"'))
   end
 
   after(:all) do
@@ -100,6 +101,11 @@ RSpec.describe 'Exporting Children as CSV', type: :feature do
     it 'Only exports submitted children' do
       unsubmitted_child_row = row_for_child @unsubmitted_child
       expect(unsubmitted_child_row).to eq(nil)
+    end
+
+    it 'Escapes double quotes' do
+      row = row_for_child @child_with_double_quotes
+      expect(row['residential_street_2']).to eq(@child_with_double_quotes.household.residential_street_2)
     end
   end
 end
