1dba6d4670591da4075b084dc86ad73f90450cde
Cory Foy <github@cornetdesign.com>
2020-06-09 17:22:48 -0400

    Updates the signature page to handle null bytes being sent

--- spec/forms/signature_form_spec.rb
+++ spec/forms/signature_form_spec.rb
@@ -13,5 +13,17 @@ describe SignatureForm do
       expect(household.signature).to eq('John Hancock')
       expect(household.submitted_at).not_to be_nil
     end
+
+    it 'handles null bytes by stripping them out' do
+      household = Household.create(is_eligible: :yes)
+      form = described_class.new(household, { signature: "John\u0000Hancock\u0000" })
+      form.valid?
+      form.save
+
+      household.reload
+
+      expect(household.signature).to eq('John Hancock')
+      expect(household.submitted_at).not_to be_nil
+    end
   end
 end
