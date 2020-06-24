4296a5e6915afec67bfe57a8d840ce4f7f5b8bfb
Ben Calegari <bcalegari@codeforamerica.org>
2020-06-23 18:01:24 -0700

    Add methods for fallback addresses on households
    [#173363902]

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,13 +1,15 @@
 class Household < ApplicationRecord
   has_many :children
 
-  enum is_eligible: { unfilled: 0, yes: 1, no: 2, dont_know: 3 }, _prefix: :is_eligible
-  enum received_card: { unfilled: 0, yes: 1, no: 2 }, _prefix: :received_card
-  enum has_mailing_address: { unfilled: 0, yes: 1, no: 2 }, _prefix: :has_mailing_address
-  enum application_experience: { unfilled: 0, good: 1, ok: 2, bad: 3 }, _suffix: true
-  enum experiment_group: { unfilled: 0, ca_early: 1 }
-  enum registered_homeless: { unfilled: 0, yes: 1 }, _prefix: :registered_homeless
-  enum same_residential_address: { unfilled: 0, yes: 1, no: 2, not_sure: 3 }, _prefix: :same_residential_address
+  enum is_eligible: {unfilled: 0, yes: 1, no: 2, dont_know: 3}, _prefix: :is_eligible
+  enum received_card: {unfilled: 0, yes: 1, no: 2}, _prefix: :received_card
+  enum has_mailing_address: {unfilled: 0, yes: 1, no: 2}, _prefix: :has_mailing_address
+  enum application_experience: {unfilled: 0, good: 1, ok: 2, bad: 3}, _suffix: true
+  enum experiment_group: {unfilled: 0, ca_early: 1}
+  enum registered_homeless: {unfilled: 0, yes: 1}, _prefix: :registered_homeless
+  enum same_residential_address: {unfilled: 0, yes: 1, no: 2, not_sure: 3}, _prefix: :same_residential_address
+
+  scope :submitted, -> { where.not(submitted_at: nil) }
 
   scope :submitted, -> { where.not(submitted_at: nil) }
   scope :unsubmitted, -> { where(submitted_at: nil) }
@@ -22,5 +24,21 @@ class Household < ApplicationRecord
 
   def not_homeless?
     registered_homeless != 'yes'
+  end
+
+  def street
+    mailing_street.or_if_blank(residential_street)
+  end
+
+  def street_2
+    mailing_street_2.or_if_blank(residential_street_2)
+  end
+
+  def city
+    mailing_city.or_if_blank(residential_city)
+  end
+
+  def zip_code
+    mailing_zip_code.or_if_blank(residential_zip_code)
   end
 end
--- spec/models/household_spec.rb
+++ spec/models/household_spec.rb
@@ -22,4 +22,52 @@ describe Household do
       expect(household.youngest_child.first_name).to eq('Joe')
     end
   end
+
+  describe '#street' do
+    it 'returns residential street' do
+      h = build(:household, residential_street: 'Fake Street')
+      expect(h.street).to eq(h.residential_street)
+    end
+
+    it 'returns mailing_street if residential_street is blank' do
+      h = build(:household, residential_street: '', mailing_street: 'Fake Mailing Street')
+      expect(h.street).to eq(h.mailing_street)
+    end
+  end
+
+  describe '#street_2' do
+    it 'returns residential_street_2' do
+      h = build(:household, residential_street_2: 'Fake Street')
+      expect(h.street_2).to eq(h.residential_street_2)
+    end
+
+    it 'returns mailing_street_2 if residential_street_2 is blank' do
+      h = build(:household, residential_street_2: '', mailing_street_2: 'Fake Mailing Street')
+      expect(h.street_2).to eq(h.mailing_street_2)
+    end
+  end
+
+  describe '#city' do
+    it 'returns residential city' do
+      h = build(:household, residential_city: 'Fake City')
+      expect(h.city).to eq(h.residential_city)
+    end
+
+    it 'returns mailing_city if residential_city is blank' do
+      h = build(:household, residential_city: '', mailing_city: 'Fake City')
+      expect(h.city).to eq(h.mailing_city)
+    end
+  end
+
+  describe '#zip_code' do
+    it 'returns residential zip_code' do
+      h = build(:household, residential_zip_code: 'Fake Zipcode')
+      expect(h.zip_code).to eq(h.residential_zip_code)
+    end
+
+    it 'returns mailing_zip_code if residential_zip_code is blank' do
+      h = build(:household, residential_zip_code: '', mailing_zip_code: 'Fake Zipcode')
+      expect(h.zip_code).to eq(h.mailing_zip_code)
+    end
+  end
 end
