eb1fa117e05f2365c8e36caf0b3935496bf86397
Luigi Ray-Montanez <luigi@codeforamerica.org>
2020-07-14 16:32:36 -0400

    Close the application when CLOSED ENV var is set 🚪

--- config/routes.rb
+++ config/routes.rb
@@ -7,17 +7,19 @@ Rails.application.routes.draw do
 
   scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
     get 'early' => 'pages#early', as: 'early'
-    get 'how' => 'pages#how', as: 'how'
     get 'info' => 'pages#info', as: 'info'
 
-    resources :steps, controller: :forms, only: (Rails.env.production? ? %i[show] : %i[show index]) do
-      collection do
-        FormNavigation.controllers.uniq.each do |controller_class|
-          { get: :edit, put: :update }.each do |method, action|
-            match "/#{controller_class.to_param}",
-                  action: action,
-                  controller: controller_class.controller_path,
-                  via: method
+    if ENV['CLOSED'] != '1'
+      get 'how' => 'pages#how', as: 'how'
+      resources :steps, controller: :forms, only: (Rails.env.production? ? %i[show] : %i[show index]) do
+        collection do
+          FormNavigation.controllers.uniq.each do |controller_class|
+            { get: :edit, put: :update }.each do |method, action|
+              match "/#{controller_class.to_param}",
+                    action: action,
+                    controller: controller_class.controller_path,
+                    via: method
+            end
           end
         end
       end
