0d387b91c952d4928d0df14c8102b741f52d637f
Ben Calegari <bcalegari@codeforamerica.org>
2020-09-23 13:45:54 -0400

    Stop sending mixpanel events

--- app/assets/javascripts/mixpanel_loader.js
+++ app/assets/javascripts/mixpanel_loader.js
@@ -1,15 +1,6 @@
 const mixpanelLoader = function (lib = mixpanel, jQuery = $) {
     return {
-        trackLinks() {
-            jQuery('a, button').click(function (e) {
-                lib.track("Click", {
-                    destination: e.currentTarget.href,
-                    text: e.currentTarget.text
-                })
-            });
-        },
-        trackPageLoad() {
-            lib.track("Page View")
-        }
+        trackLinks() {},
+        trackPageLoad() {}
     }
 }
